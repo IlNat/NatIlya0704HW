2db2c418289a28534b50fd3125dc4214b2aa10a2
IlNat <103373903+IlNat@users.noreply.github.com>
2022-04-10 22:38:35 +0500

    Add files via upload

---
+++ NatIlya0704HWzad1.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+template<typename T>
+int swapnum(T& c, T& d) 
+{
+    T temp = c;
+    c = d;
+    d = temp;
+    return c, d;
+}
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    swapnum(a, b);
+    cout << a << " " << b << '\n';
+}
---
+++ NatIlya0704HWzad2.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <ctime>
+using namespace std;
+template <typename T>
+void print(T* str, T* fin) {
+    while (str != fin) {
+        cout << *str << ' ';
+        ++str;
+    }
+    cout << '\n';
+}
+
+void fill(int b[], int size) {
+    srand(time(0));
+    for (int i = 0; i < size; i++) {
+        b[i] = rand() % 100;
+        cout << b[i] << ' ';
+    }
+    cout << '\n';
+}
+
+int main()
+{
+    const int size = 10;
+    int a[size];
+    fill(a, size);
+    print(a, a + 10);
+    cout << '\n';
+}
